2cf92c61ee214ddcb1b2e2e2ed19b694ece8a14e
kaike <36178392+kaikealvesdesouza@users.noreply.github.com>
2023-06-01 19:14:52 -0300

    Add files via upload

--- sites.js
+++ sites.js
@@ -1,8 +1,9 @@
 
 var sites = {
-	"amazon":{"title":"amazon",abstract:"um site de venda de produtos",url:"amazon.com.br","search":"s","params":[{"name":"k",f:"search"},{"name":"__mk_pt_BR",f:"n",v:"%C3%85M%C3%85%C5%BD%C3%95%C3%91"}]},
-	"youtube":{"title":"youtube",abstract:"um site de videos postados por usuarios",url:"youtube.com.br","search":"results","params":[{"name":"search_query",f:"search"}]},
-  "wikipedia":{title:"wikipedia",abstract:"site de conhecimento universal",url:"wikipedia.org",search:"wiki",params:[]}
+	"amazon":{"title":"amazon",abstract:"um site de venda de produtos",url:"amazon.com.br","search":"s","params":[{"name":"k",f:"search"},{"name":"__mk_pt_BR",f:"n",v:"%C3%85M%C3%85%C5%BD%C3%95%C3%91"}],keywords:["comprar","shop"]},
+	"youtube":{"title":"youtube",abstract:"um site de videos postados por usuarios",url:"youtube.com.br","search":"results","params":[{"name":"search_query",f:"search"}],keywords:["video","streaming","noticia"]},
+  "wikipedia":{title:"wikipedia",abstract:"site de conhecimento universal",url:"wikipedia.org",search:"wiki",params:[],keywords:["significado","artigo","conceito","definição","resumo","o que é?","de onde veio?","como?","porque?"]},
+	"stackoverflow":{title:"stackoverflow",abstract:"forum de programação",url:"stackoverflow.com",search:"search",params:[{name:"q",f:"search"}],keywords:["programação","program","programmer","javascript","css","html","f","python","C#","PHP","pascal","ruby","asp.net"]}
 };
 		
 
