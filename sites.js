292cc9d5bf121a0af799178590741b6ad621bbe7
kaike <36178392+kaikealvesdesouza@users.noreply.github.com>
2023-06-04 06:06:33 -0300

    Add files via upload

--- sites.js
+++ sites.js
@@ -18,6 +18,7 @@ var sites = {
   "flickr":{title:"flickr",url:"flickr.com",abstract:"o melhor aplicativo online de gerenciamento e compartilhamento no mundo",search:"search",params:[{name:"text",f:"search"}],keywords:["fotos","photos","imagens","images","imagem","retrato","foto"]},
   "marvelcinematicuniverse":{title:"marvelcinematicuniverse",url:"marvelcinematicuniverse.fandom.com/wiki/",abstract:"The online marvel cinematic universe encyclopedia",search:"Special:Search",params:[{name:"query",f:"search"}],keywords:["marvel","fotos","photos","photo","foto"]},
 	"bibliotecanacional":{title:"bibliotecanacional",url:"bndigital.bn.gov.br/bibliotecas-digitais-pelo-mundo/",search:"",params:[],keywords:["livro","acervo","biblioteca","jornal","book"]}
+	//https://institutocpfl.org.br/?s=eugenio&submit=
 };
 		
 
